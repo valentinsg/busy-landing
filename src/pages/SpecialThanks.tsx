69cca42a61a1ea57e74689149c7a6e14f61cbd82
valentinsg <sanchezguevaravalentin@gmail.com>
2024-09-25 23:15:06 -0300

    probar special thanks responsive

--- src/pages/SpecialThanks.tsx
+++ src/pages/SpecialThanks.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Flex, Text, Button, VStack, Heading, useColorMode } from '@chakra-ui/react';
+import { Flex, Text, Button, VStack, useColorMode} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
 
 const SpecialThanks = () => {
   const { colorMode } = useColorMode();
@@ -10,29 +11,23 @@ const SpecialThanks = () => {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      minHeight="100vh"
+      minHeight="95vh"
       p={8}
       textAlign="center"
       bg={colorMode === "dark" ? "brand.dark" : "brand.light"}
     >
-      <VStack spacing={8} maxWidth="800px">
-        <Heading as="h1" size="2xl" mb={6} fontFamily="heading">
-          Gracias por tu interés en Busy
-        </Heading>
-        <Text fontSize="xl" lineHeight="tall" fontFamily="text">
-          Primero que nada, gracias, en nombre de toda la gente que trabaja para que Busy exista.
-          Gracias por tomarte el tiempo de escanear este QR y llegar hasta acá.
-        </Text>
-        <Text fontSize="xl" lineHeight="tall" fontFamily="text">
+        <Logo />
+      <VStack spacing={8} maxWidth="890px">
+        <Text fontSize="2xl" lineHeight="tall" fontFamily="text">
           Si te interesa saber quiénes somos, qué hacemos y cómo lo hacemos, te invitamos a que continúes explorando.
+          Siempre tenemos algo preparado para la gente busy
         </Text>
         <Link to="/home">
           <Button 
-            colorScheme="teal" 
             size="lg" 
             fontFamily="buttons"
-            px={8}
-            py={6}
+            px={12}
+            py={8}
             fontSize="xl"
             _hover={{ 
               transform: 'translateY(-2px)', 
@@ -40,7 +35,7 @@ const SpecialThanks = () => {
             }}
             transition="all 0.2s"
           >
-            Continuar
+            Seguir explorando
           </Button>
         </Link>
       </VStack>
